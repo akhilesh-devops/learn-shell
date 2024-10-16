c88d463601fc9cd1bb23507f3bf75496d0720c9d
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-16 20:13:27 +0530

    shell-script

--- function.sh
+++ function.sh
@@ -1,10 +1,3 @@
-test() {
-  echo -e "\e[36m >>>>>>  Hello world  <<<<<<<<<< \e[0m"
-  echo "My DOB is $(date)"
-  echo Total Expenses is $((2500+6500))
-}
-
-test
 
 input() {
   echo Script Name - $0
