5692413003eb915b31ab19802b140b5b9a525dea
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-16 20:15:04 +0530

    shell-script

--- function.sh
+++ function.sh
@@ -1,10 +1,10 @@
 
 input() {
-  echo Script Name - $0
-  echo First Argument - $1
-  echo Second Argument - $2
-  echo All Arguments - $*
-  echo No of Arguments - $#
+  echo "Script Name - $0"
+  echo "First Argument - $1"
+  echo "Second Argument - $2"
+  echo "All Arguments - $*"
+  echo "No of Arguments - $#"
 }
 
 input
