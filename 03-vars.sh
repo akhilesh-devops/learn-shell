3741bc9ba0e5c99e3e358cb91f969af44b76592b
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-19 09:19:03 +0530

    shell-script

--- 03-vars.sh
+++ 03-vars.sh
@@ -3,12 +3,16 @@ a=10
 #Access the variable with ${}
 echo -e "\e[32mvalue of a is ${a}\e[0m"
 
+#Special Variables
 echo script name - $0
 echo First Argument - $1
 echo Second Argument - $2
 echo No. of Arguments - $#
 echo All Arguments - $*
 
-#Command Substitution
-
+#Command Date Substitution
 echo -e "\e[36mTodays Date is $(date)\e[0m"
+
+#Arithemetic operations
+
+echo "Total sum is $(( 2+5 ))"
