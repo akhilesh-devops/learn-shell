17b750c461e5a3ddd41a7a23a9a7c2275237778b
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-19 10:19:48 +0530

    shell-script

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,8 +1,10 @@
+
+message=Function_break
 input () {
   echo -e "\e[32mFirst Command\e[0m"
   echo -e "\e[32mSecond Command\e[0m"
   echo -e "\e[32mThird Command\e[0m"
-  return 1
+  return "${message}"
   echo -e "\e[32mFourth Command\e[0m"
 }
 
