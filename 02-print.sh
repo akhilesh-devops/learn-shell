16be7ba41204b0be386b9339e516ec6f4574406b
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-19 09:00:42 +0530

    shell-script

--- 02-print.sh
+++ 02-print.sh
@@ -1,11 +1,11 @@
 echo Hello world
 echo "Hello world"
 
-Red - 31
-Green - 32
-Yellow - 33
-Blue - 34
-Magenta - 35
-Cyan - 36
+#Red - 31
+#Green - 32
+#Yellow - 33
+#Blue - 34
+#Magenta - 35
+#Cyan - 36
 
 echo -e "\e[32m >>>>>>>>> Roboshop Project <<<<<<<<<<\e[0m"
