92fe5cd922373abea58b8c4eb15426041db1c234
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-19 09:55:31 +0530

    shell-script

--- 05-conditions.sh
+++ 05-conditions.sh
@@ -1,6 +1,14 @@
 password=$1
+
 if [ -z "${password}" ]; then
   echo Input Password is Missing
 fi
 
 echo app password - ${password}
+
+
+a=10
+
+if [ $a -gt 2 ]; then
+  echo a is greater than 2
+fi
