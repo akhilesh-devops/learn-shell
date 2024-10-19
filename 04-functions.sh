6426aa2c9fc5df6c37231b8681d804f690052734
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-19 10:18:25 +0530

    shell-script

--- 04-functions.sh
+++ 04-functions.sh
@@ -2,7 +2,7 @@ input () {
   echo -e "\e[32mFirst Command\e[0m"
   echo -e "\e[32mSecond Command\e[0m"
   echo -e "\e[32mThird Command\e[0m"
-  exit 1
+  return 1
   echo -e "\e[32mFourth Command\e[0m"
 }
 
