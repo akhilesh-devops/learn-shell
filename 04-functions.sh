478fc9f398b2f7ecd522231de57073db39177189
AKhilesh <kothapallyakhil123@gmail.com>
2024-10-19 09:30:44 +0530

    shell-script

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,8 +1,8 @@
 input () {
-  echo "First Command"
-  echo "Second Command"
-  echo "Third Command"
-  echo "Fourth Command"
+  echo "\e[32mFirst Command\e[0m"
+  echo "\e[32mSecond Command\e[0m"
+  echo "\e[32mThird Command\e[0m"
+  echo "\e[32mFourth Command\e[0m"
 }
 
 input
