4f5ae6eb368fbb6b731892fd48143b9897216813
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 13:43:52 +0530

    test

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,7 +1,7 @@
 input() {
   echo -e "\e[32mFirst command - $1\e[0m"
   echo -e "\e[32mSecond command - $2\e[0m"
-  echo -e "\e[32mThird command - $3\e[0m"
+  echo -e "\e[32m"Third command - $3\e[0m"
 }
 
 input 123 456 789
