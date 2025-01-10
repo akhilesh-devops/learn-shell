449bd2b4e563552d2512222c50cb005026758c34
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 12:51:26 +0530

    test

--- 02-print.sh
+++ 02-print.sh
@@ -1,3 +1,13 @@
 echo Hello world
 echo ">>>>>>>> Hello world <<<<<<<<<<<<<"
-echo -e "\e[31m >>>>>>> Welcome to DevOps <<<<<<<<<\e[0m"
+echo -e "\e[34m >>>>>>> Welcome to DevOps <<<<<<<<<\e[0m"
+
+### colour codes
+
+#Red - 31
+#Yellow - 32
+#Green - 33
+#Blue - 34
+#Magenta - 35
+#Cyan - 36
+
