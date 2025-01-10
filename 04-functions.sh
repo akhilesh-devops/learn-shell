3db626399d77835b951190bbef3360e1f982a524
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 13:34:17 +0530

    test

--- 04-functions.sh
+++ 04-functions.sh
@@ -1,21 +1,22 @@
-input () {
-  echo -e "\e[32mFirst Command\e[0m"
-  echo -e "\e[32mSecond Command\e[0m"
-  echo -e "\e[32mThird Command\e[0m"
-  return 123
-  echo -e "\e[32mFourth Command\e[0m"
+input() {
+  echo -e "\e[32mFirst command - $1\e[0m"
+  echo -e "\e[32mSecond command - $2\e[0m"
+  echo -e "\e[32mThird command - $3\e[0m"
 }
 
-input
+input abc xyz pqr
 
-echo "Function Exit Status - $?"
 
-test () {
-  echo "First Argument - $1"
-  echo "Second Argument - $2"
-  echo "Third Argument - $3"
-  echo "Fourth Argument - $4"
-  echo "Script Name - $0"
-}
 
-test 123 456 789 852
+
+#echo "Function Exit Status - $?"
+#
+#test () {
+#  echo "First Argument - $1"
+#  echo "Second Argument - $2"
+#  echo "Third Argument - $3"
+#  echo "Fourth Argument - $4"
+#  echo "Script Name - $0"
+#}
+#
+#test 123 456 789 852
