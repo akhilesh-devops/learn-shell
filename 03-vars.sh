3810b99c4aa5e3739ce12b546f27081e7e113b53
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 12:57:27 +0530

    test

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,13 +1,13 @@
 a=10
 echo a is ${a}
 
-##Special Variables
-#echo script name - $0
-#echo First Argument - $1
-#echo Second Argument - $2
-#echo No. of Arguments - $#
-#echo All Arguments - $*
-#
+#Special Variables
+echo script name - $0
+echo First Argument - $1
+echo Second Argument - $2
+echo No. of Arguments - $#
+echo All Arguments - $*
+
 ##Command Date Substitution
 #echo -e "\e[36mTodays Date is $(date)\e[0m"
 #
