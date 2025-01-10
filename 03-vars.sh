2daf66f554fe6a9864103e108226dec769cfab46
AKhilesh <kothapallyakhil123@gmail.com>
2025-01-10 12:55:47 +0530

    test

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,24 +1,23 @@
 a=10
 
-#Access the variable with ${}
-echo -e "\e[32mvalue of a is ${a}\e[0m"
+echo a is ${a}
 
-#Special Variables
-echo script name - $0
-echo First Argument - $1
-echo Second Argument - $2
-echo No. of Arguments - $#
-echo All Arguments - $*
-
-#Command Date Substitution
-echo -e "\e[36mTodays Date is $(date)\e[0m"
-
-#Arithemetic operations
-
-echo "Total sum is $(( 2+5 ))"
-
-echo "value of a is $(($a-1))"
-
-# Environment Variables
-
-echo username is $USER
+##Special Variables
+#echo script name - $0
+#echo First Argument - $1
+#echo Second Argument - $2
+#echo No. of Arguments - $#
+#echo All Arguments - $*
+#
+##Command Date Substitution
+#echo -e "\e[36mTodays Date is $(date)\e[0m"
+#
+##Arithemetic operations
+#
+#echo "Total sum is $(( 2+5 ))"
+#
+#echo "value of a is $(($a-1))"
+#
+## Environment Variables
+#
+#echo username is $USER
